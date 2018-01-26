7f392e2b0e0048a13173e58a62e9079924c93c29
pollockcl <pollock.clee@gmail.com>
2018-01-26 08:33:26 -0700

    added start method for iteration 5.

--- lib/round.rb
+++ lib/round.rb
@@ -1,9 +1,11 @@
+require 'pry'
 class Round
   attr_reader :deck, :guesses
 
   def initialize(deck)
     @deck         = deck
     @guesses      = []
+    @feedback     = nil
   end
   
   def current_card
@@ -13,6 +15,7 @@ class Round
   def record_guess(guess)
     temp = Guess.new(guess, current_card)
     @guesses << temp
+    @feedback = temp.feedback
     temp
   end
 
@@ -22,6 +25,25 @@ class Round
 
   def percent_correct
     ((number_correct.to_f / @guesses.size) * 100).round
+  end
+
+  def start
+    iteration = 1
+    puts "Welcome, You're playing with #{@deck.count} cards."
+    puts '--------------------------------------------------'
+
+    until iteration > @deck.count
+      puts "This is card number #{@guesses.size + 1}"
+      puts current_card.question
+      guess = gets.chomp
+      record_guess(guess)
+      puts @feedback
+      iteration += 1
+    end
+    
+    puts "****** Game over! ******"
+    puts "You had #{number_correct} correct guesses out of 4 for a score of #{percent_correct}\%."
+
   end
 
 end
